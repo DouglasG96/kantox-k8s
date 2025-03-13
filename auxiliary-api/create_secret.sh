cdbce60e933eede5ef6d9c7de4c53f938332c9ce
Douglas Guzman <drgb96@gmail.com>
2025-03-12 20:05:34 -0600

    Adding secret for pulling images and script for creating it

---
+++ auxiliary-api/create_secret.sh
@@ -0,0 +1,6 @@
+kubectl create secret docker-registry regcred \
+  --docker-server=docker.io \
+  --docker-username=your-dockerhub-username \
+  --docker-password=your-dockerhub-password \
+  --docker-email=your-dockerhub@email.com \
+  -n auxiliary-service-prod
